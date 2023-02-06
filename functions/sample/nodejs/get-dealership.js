a23cdb8b83be17f9b6a56bea2e983563f7d204d1
Bakul Mukherjee <98204727+bakulm4@users.noreply.github.com>
2023-02-05 19:40:00 -0800

    Add models for Car Make and Model

---
+++ functions/sample/nodejs/get-dealership.js
@@ -0,0 +1,80 @@
+/**
+  *
+  * main() will be run when you invoke this action
+  *
+  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+  *
+  * @return The output of this action, which must be a JSON object.
+  *
+  */
+ const { CloudantV1 } = require('@ibm-cloud/cloudant');
+ const { IamAuthenticator } = require('ibm-cloud-sdk-core');
+ 
+ function main(params) {
+     
+     const IAM_API_KEY = 'VHPowScjpMEuMvqdJHGV7t2vyhoQP_sWukDkhlJ-OOga';
+     const COUCH_URL = 'https://32b60b7a-3008-49d8-9c8a-e9c4703fd00f-bluemix.cloudantnosqldb.appdomain.cloud';
+     const DB_NAME = 'dealerships'
+     
+     const authenticator = new IamAuthenticator({ apikey: IAM_API_KEY });
+     
+     const cloudant = CloudantV1.newInstance({
+       authenticator: authenticator
+     });
+     cloudant.setServiceUrl(COUCH_URL);
+     
+     if(params.st)
+         return getMatchingRecords(cloudant,DB_NAME,{st:params.st});
+         
+     return getAllRecords(cloudant,DB_NAME);
+ }
+ 
+ function getMatchingRecords(cloudant,dbname, selector) {
+    return new Promise((resolve, reject) => {
+        cloudant.postFind({db:dbname,selector:selector})
+                .then((result)=>{
+                 console.log(`Get Matching Records: `,result.result.docs);
+                   let code = 200;
+                   if (result.result.docs.length === 0) {
+                       code = 404;
+                   }
+                  resolve({
+                   statusCode: code,
+                   headers: { 'Content-Type': 'application/json' },
+                   result:result.result.docs});
+                })
+                .catch(err => {
+                   console.log(err);
+                    reject({
+                       statusCode: 500,
+                       err: `Something went wrong: ${err.message}` 
+                     });
+                });
+         })
+}
+ 
+  function getAllRecords(cloudant,dbname) {
+      return new Promise((resolve, reject) => {
+          cloudant.postAllDocs({ db: dbname, includeDocs: true, limit: 10 })            
+              .then((result)=>{
+                 console.log(`getAllRecords - result: `,result.result.rows);
+                   let code = 200;
+                   if (result.result.rows.length === 0) {
+                       code = 404;
+                   }
+                resolve({
+                    statusCode: code,
+                    headers: { 'Content-Type': 'application/json' },
+                    result:result.result.rows
+                });
+              })
+              .catch(err => {
+                 console.log(err);
+                 reject({
+                     statusCode: 500,
+                     err: `Something went wrong: ${err.message}` 
+                 });
+              });
+          })
+  }
+  
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,6 +9,12 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(blank=False,max_length=15)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return f'Make: {self.name}'
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -19,9 +25,59 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'SD'
+    SUV = 'SV'
+    MINIVAN = 'MV'
+    WAGON = 'WG'
+    TRUCK = 'TR'
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'Suv'),
+        (MINIVAN, 'Minivan'),
+        (WAGON, 'Wagon'),
+        (TRUCK, 'Truck'),
+    ]
+    type = models.CharField(
+        max_length=2,
+        choices=CAR_TYPE_CHOICES,
+        default=SEDAN,
+    )
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    name = models.CharField(blank=False,max_length=15)
+    dealer_id = models.IntegerField(blank=True)
+    year = models.DateField(null=True)
 
+    def __str__(self):
+        return f'Model: {self.name} Make: {self.car_make}'
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
+class CarDealer:
+
+    def __init__(self, address, city, full_name, id, lat, long, short_name, st, state, zip):
+        # Dealer address
+        self.address = address
+        # Dealer city
+        self.city = city
+        # Dealer Full Name
+        self.full_name = full_name
+        # Dealer id
+        self.id = id
+        # Location lat
+        self.lat = lat
+        # Location long
+        self.long = long
+        # Dealer short name
+        self.short_name = short_name
+        # Dealer state abbrev
+        self.st = st
+        # Dealer state abbrev
+        self.state = state
+        # Dealer zip
+        self.zip = zip
+
+    def __str__(self):
+        return "Dealer name: " + self.full_name
 
 
 # <HINT> Create a plain Python class `DealerReview` to hold review data
